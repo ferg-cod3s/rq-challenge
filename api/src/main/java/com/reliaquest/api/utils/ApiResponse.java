96fd9e649803cb3cd9fe4852024d5bf8ac893d3c
John Ferguson <johncferguson90@gmail.com>
2025-08-17 05:32:54 -0600

    refactor ApiResponse class to use Lombok for getters and remove redundant methods

--- api/src/main/java/com/reliaquest/api/utils/ApiResponse.java
+++ api/src/main/java/com/reliaquest/api/utils/ApiResponse.java
@@ -1,28 +1,11 @@
 package com.reliaquest.api.utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter
 public class ApiResponse<T> {
     private T data;
     private String status;
-
-    public T getData() {
-        return data;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    // Overloaded method for list responses - returns empty list if data is null
-    @SuppressWarnings("unchecked")
-    public List<T> getDataAsList() {
-        if (data instanceof List) {
-            return (List<T>) data;
-        }
-        return new ArrayList<>();
-    }
 }
